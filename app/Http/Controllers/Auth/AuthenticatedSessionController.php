c8be1296cd964898fe56a708d1076cc2d14b659e
Juan Zamora <uraharazamora@gmail.com>
2025-02-04 15:22:38 -0400

    User logeo

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -38,7 +38,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/');
+        return redirect()->intended('/dashboard');
     }
 
     /**
